c76e2979497c1a11b0bd0d152735e3ba7d5ad160
Benjamin Cane <ben@bencane.com>
2017-12-25 00:43:03 -0700

    Fixing flags descriptions

--- main.go
+++ main.go
@@ -10,11 +10,11 @@ import (
 
 type options struct {
 	Verbose  bool   `short:"v" long:"verbose" description:"Enable verbose output"`
-	Efs2file string `short:"f" long:"file" description:"Specify an alternative Efs2file (default: ./Efs2file)" default:"./Efs2file"`
+	Efs2file string `short:"f" long:"file" description:"Specify an alternative Efs2file" default:"./Efs2file"`
 	Keyfile  string `short:"i" long:"key" description:"Specify an SSH Private key to use (default: ~/.ssh/id_rsa)"`
-	Parallel bool   `short:"p" long:"parallel" description:"Execute tasks in parallel (default: false)"`
+	Parallel bool   `short:"p" long:"parallel" description:"Execute tasks in parallel"`
 	Dryrun   bool   `short:"d" long:"dryrun" description:"Print tasks to be executed without actually executing any tasks"`
-	Port     string `long:"port" description:"Define an alternate SSH Port (default: 22)" default:"22"`
+	Port     string `long:"port" description:"Define an alternate SSH Port" default:"22"`
 	User     string `short:"u" long:"user" description:"Remote host username (default: current user)"`
 }
 
